e7f6cee5be3709f8ee6ba2a1d3b71e88da6ff2c9
Muhammad Iqbal <71365593+sirdar9009@users.noreply.github.com>
2020-09-21 10:05:03 +0700

    Add files via upload

---
+++ tugas15.js
@@ -0,0 +1,11 @@
+var AB=8, BC=6
+
+function akar(nilai) {
+	return Math.sqrt(nilai)
+}
+
+function pangkat(nilai,pangkat) {
+	return Math.pow(nilai,pangkat)
+}
+
+console.log(akar(pangkat(AB,2)+pangkat(BC,2)))
